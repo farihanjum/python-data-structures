32810e102d561ebfe9635de2422dd14dccc443eb
farihanjum <farihaanjum.24csedu.022@gmail.com>
2020-05-16 22:52:44 +0600

    2

---
+++ week 1/counting a's in banana.py
@@ -0,0 +1,6 @@
+name = 'banana'
+tot= 0
+for i in name :
+    if i == 'a' :
+        tot +=1
+print(tot)
--- week 1/try looping through string.py
+++ week 1/try looping through string.py
@@ -3,3 +3,7 @@ i= 0
 while i < len(name) :
     print(i , name[i])
     i+=1
+#or for diyeo kora jay
+i=0
+for i in name :
+    print(i)
